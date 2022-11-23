575fd5810acc58042474f5e1932a12b993da171a
PhamTrung1993 <108708120+PhamTrung1993@users.noreply.github.com>
2022-11-23 17:06:41 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WordListComponent} from "./dictionary/word-list/word-list.component";
+import {WordDetailComponent} from "./dictionary/word-detail/word-detail.component";
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'dictionary/list',
+  component: WordListComponent,
+  children: [{
+    path: ':id',
+    component: WordDetailComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WordListComponent } from './dictionary/word-list/word-list.component';
+import { WordDetailComponent } from './dictionary/word-detail/word-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordListComponent,
+    WordDetailComponent
   ],
   imports: [
     BrowserModule,
